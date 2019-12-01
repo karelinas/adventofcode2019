dfbd508bfe9ee75a6d4ee0226e46b6756c19ed05
karelinas <45606302+karelinas@users.noreply.github.com>
2019-12-01 14:04:03 +0200

    Clean up day 1 solution

--- day01/a.py
+++ day01/a.py
@@ -2,15 +2,15 @@
 #
 # Run with: python3 a.py < input.txt
 
-import math
 import sys
 
-def fuel_required(module_mass):
-    return math.floor(module_mass / 3) - 2
+def fuel_for_mass(module_mass):
+    return module_mass // 3 - 2
 
-assert fuel_required(12) == 2
-assert fuel_required(14) == 2
-assert fuel_required(1969) == 654
-assert fuel_required(100756) == 33583
+assert fuel_for_mass(12) == 2
+assert fuel_for_mass(14) == 2
+assert fuel_for_mass(1969) == 654
+assert fuel_for_mass(100756) == 33583
 
-print(sum((fuel_required(int(line.strip())) for line in sys.stdin)))
+if __name__ == '__main__':
+    print(sum((fuel_for_mass(int(line)) for line in sys.stdin)))
--- day01/b.py
+++ day01/b.py
@@ -2,11 +2,8 @@
 #
 # Run with: python3 b.py < input.txt
 
-import math
 import sys
-
-def fuel_for_mass(mass):
-    return math.floor(mass / 3) - 2
+from a import fuel_for_mass
 
 def total_fuel_required(mass):
     added_fuel = fuel_for_mass(mass)
@@ -14,15 +11,11 @@ def total_fuel_required(mass):
         return 0
     return added_fuel + total_fuel_required(added_fuel)
 
-assert fuel_for_mass(12) == 2
-assert fuel_for_mass(14) == 2
-assert fuel_for_mass(1969) == 654
-assert fuel_for_mass(100756) == 33583
-
 assert total_fuel_required(14) == 2
 assert total_fuel_required(1969) == 966
 assert total_fuel_required(100756) == 50346
 
-fuel_required = sum((total_fuel_required(int(line.strip())) for line in sys.stdin))
-print(fuel_required)
+if __name__ == '__main__':
+    fuel_required = sum((total_fuel_required(int(line)) for line in sys.stdin))
+    print(fuel_required)
 
