2f81f966b35d22605e415e5690d8324c4f867e27
karelinas <45606302+karelinas@users.noreply.github.com>
2019-12-08 15:07:14 +0200

    Day 8: run autopep8

--- day08/main.py
+++ day08/main.py
@@ -4,10 +4,11 @@
 
 import sys
 
-IMAGE_SIZE=(25, 6)
+IMAGE_SIZE = (25, 6)
 BLACK = 0
 WHITE = 1
 TRANSPARENT = 2
+
 
 def main():
     # Part 1
@@ -20,22 +21,27 @@ def main():
     image = compose_image(layers)
     print_image(image, IMAGE_SIZE[0])
 
+
 def read_raw_image(strdata):
-     return [int(x) for x in strdata]
+    return [int(x) for x in strdata]
+
 
 def read_layers(data, width, height):
     layer_size = width * height
     return [data[start:start+layer_size]
             for start in range(0, len(data), layer_size)]
 
+
 def checksum(layers):
     fewest_zeros = min(layers, key=lambda x: x.count(0))
     return fewest_zeros.count(1) * fewest_zeros.count(2)
+
 
 def compose_image(layers):
     def first_nontransparent(px):
         return px[0] if px[0] != TRANSPARENT else first_nontransparent(px[1:])
     return [first_nontransparent(px) for px in zip(*layers)]
+
 
 def print_image(image, width):
     for idx, px in enumerate(image):
@@ -43,6 +49,7 @@ def print_image(image, width):
             print()
         print('#' if px == WHITE else ' ', end='')
     print()
+
 
 if __name__ == '__main__':
     main()
